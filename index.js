49ae11fe4005231dae118392fb6e71975795207a
Akiko Green <agreen@alphaworks.tech>
2022-04-11 14:13:20 -0700

    feat(Re-doing entire design. Make more sense as buttons)

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-console.log("hi")
 
 let submit = document.querySelector("button")
 let radioInputs = document.querySelectorAll("input[name='rating']")
 let output = document.getElementById("output")
 let form = document.getElementById("rating-form")
 
-console.log(form)
+//thank you page
+function thankYouPage(){
+  let p = document.createElement("p")
+  const choosenRate = document.createTextNode(`You selected ${radioInputs.checked}`)
+  p.appendChild(choosenRate)
+
+if(radioInputs.checked === true){
+  console.log(p)
+  window.location.replace('./thankyou.html')
+} else {
+  console.log('nothing')
+}
+  
+}
+thankYouPage()
+
 
 submit.addEventListener("click", (e) =>{
   e.preventDefault()
   let selectRating;
-
-
   for(const radioInput of radioInputs){
     if(radioInput.checked){
+      // console.log(radioInput.checked === true ? console.log(radioInput.value) : "nothing")
       selectRating = radioInput.value
       break;
     }
   }
   if(selectRating){
-    // alert(selectRating)
-    window.location.replace("./thankyou.html")
+    alert(selectRating)
+    thankYouPage()
 
   } else {
     alert("select something")
@@ -28,3 +41,4 @@ submit.addEventListener("click", (e) =>{
   // form.submit()
   // output.innerText = selectRating ? `you selected ${selectRating}` : `choose a rating`
 })
+
