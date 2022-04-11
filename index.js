eab0a03f4a97be45805c993a5ef6fdca0c9b9043
Akiko Green <agreen@alphaworks.tech>
2022-04-11 14:36:33 -0700

    fix(Buttons have click function added to each )

--- index.js
+++ index.js
@@ -1,44 +1,15 @@
+let submit = document.querySelector("button");
+let rateButtons = document.getElementsByClassName("rating");
+console.log(rateButtons);
 
-let submit = document.querySelector("button")
-let radioInputs = document.querySelectorAll("input[name='rating']")
-let output = document.getElementById("output")
-let form = document.getElementById("rating-form")
-
-//thank you page
-function thankYouPage(){
-  let p = document.createElement("p")
-  const choosenRate = document.createTextNode(`You selected ${radioInputs.checked}`)
-  p.appendChild(choosenRate)
-
-if(radioInputs.checked === true){
-  console.log(p)
-  window.location.replace('./thankyou.html')
-} else {
-  console.log('nothing')
-}
-  
-}
-thankYouPage()
-
-
-submit.addEventListener("click", (e) =>{
-  e.preventDefault()
-  let selectRating;
-  for(const radioInput of radioInputs){
-    if(radioInput.checked){
-      // console.log(radioInput.checked === true ? console.log(radioInput.value) : "nothing")
-      selectRating = radioInput.value
-      break;
-    }
+//we need to add a click funtion to our buttons
+function selectRateButton() {
+  for (let i = 0; i < rateButtons.length; i++) {
+    rateButtons[i].addEventListener("click", () => {
+      console.log("click");
+    });
   }
-  if(selectRating){
-    alert(selectRating)
-    thankYouPage()
+}
+selectRateButton();
 
-  } else {
-    alert("select something")
-  }
-  // form.submit()
-  // output.innerText = selectRating ? `you selected ${selectRating}` : `choose a rating`
-})
-
+//add an active funtion to button thats clicked
