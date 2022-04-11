e6d7d61e4ba7993a843284031f293e1f518bbe06
Akiko Green <agreen@alphaworks.tech>
2022-04-11 11:30:39 -0700

    feat:(Alerts user of rating click. Directs user to Thank you component)

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 console.log("hi")
+
+let submit = document.querySelector("button")
+let radioInputs = document.querySelectorAll("input[name='rating']")
+let output = document.getElementById("output")
+let form = document.getElementById("rating-form")
+
+console.log(form)
+
+submit.addEventListener("click", (e) =>{
+  e.preventDefault()
+  let selectRating;
+
+
+  for(const radioInput of radioInputs){
+    if(radioInput.checked){
+      selectRating = radioInput.value
+      break;
+    }
+  }
+  if(selectRating){
+    // alert(selectRating)
+    window.location.replace("./thankyou.html")
+
+  } else {
+    alert("select something")
+  }
+  // form.submit()
+  // output.innerText = selectRating ? `you selected ${selectRating}` : `choose a rating`
+})
