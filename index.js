c62e0e97745aedb15e0aa67c7e7e72d202f83c0f
Akiko Green <agreen@alphaworks.tech>
2022-04-18 12:05:25 -0700

    testng

--- index.js
+++ index.js
@@ -1,54 +1,60 @@
 let submit = document.getElementById("submit-btn");
 let rateButtons = document.getElementsByClassName("rating");
 let showAnswer = document.getElementById("showAnswer");
-let ratingCard = document.getElementById("card");
+let ratingCard = document.getElementById("rating-card");
 let thankYouCard = document.getElementById("thank-you-card");
-console.log(submit);
 let response = "";
-
 
 //we need to add a click funtion to our buttons
 function selectRateButton() {
   for (let i = 0; i < rateButtons.length; i++) {
-    rateButtons[i].addEventListener("click", activeRateClicked, false);
+    rateButtons[i].addEventListener("click", () => {
+      activeRateClicked(rateButtons[i])
+    });
   }
 }
 selectRateButton();
 
 //add an active funtion to button thats clicked
-
-function activeRateClicked() {
-  resetButton();
-  this.classList.toggle("active");
-  response = this.id;
-
-  // if(response !== "active"){
-  //   this.classList.toggle("active")
-  // } else{
-  //   resetButton()
-  // }
+function activeRateClicked(element) {
+  // debugger
+console.log(element)
+//toggle allows to auto show or remove show
+  element.classList.toggle("show");
+  response = element.innerText
+ 
+  console.log(response)
 }
 
-//reset the button so a different one looks activated
-function resetButton() {
-  for (let i = 0; i < rateButtons.length; i++) {
-    rateButtons[i].classList.remove("active");
-  }
-  return response = "";
-}
+
+
 
 //submit button functionality
-submit.addEventListener("click", () => {
-  submit.classList.add("submit-active");
-  submit.classList.add("submit-text-active")
-  showResponse();
-});
-console.log(ratingCard)
+function submitRating() {
+  submit.addEventListener("click", () => {
 
+    thankYouCard.classList.remove("hide")
+    ratingCard.classList.add("hide")
+    let showRating = `You selected ${response} out of 5`;
+    return showAnswer.innerText = showRating;
+  
 
-function showResponse() {
-  ratingCard.classList.add("hidden");
-  thankYouCard.classList.remove("hidden");
-  let showRating = `You selected ${response} out of 5`;
-  showAnswer.innerText = showRating;
+    // showResponse();
+  });
 }
+submitRating();
+
+
+// function showResponse() {
+// console.log(response)
+
+//   thankYouCard.classList.remove("hide")
+//   ratingCard.classList.add("hide")
+//   let showRating = `You selected ${response} out of 5`;
+//   return showAnswer.innerText = showRating;
+
+
+// }
+
+// showResponse()
+
