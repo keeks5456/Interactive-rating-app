e9cf350048f02d24a6dc76107ec4e6b05d7ce41c
Akiko Green <agreen@alphaworks.tech>
2022-05-17 16:27:33 -0700

    refactor(Active /Inactive Button)

--- index.js
+++ index.js
@@ -1,39 +1,32 @@
 let submit = document.getElementById("submit-btn");
-let rateButtons = document.getElementsByClassName("rating");
+let rateButtons = document.querySelectorAll(".rating");
 let showAnswer = document.getElementById("showAnswer");
 let ratingCard = document.getElementById("rating-card");
 let thankYouCard = document.getElementById("thank-you-card");
 let response = "";
 
 //we need to add a click funtion to our buttons
-function selectRateButton() {
-  for (let i = 0; i < rateButtons.length; i++) {
-    rateButtons[i].addEventListener("click", () => {
-      activeRateClicked(rateButtons[i]);
-    });
-  }
-}
-selectRateButton();
 
-//add an active funtion to button thats clicked
-function activeRateClicked(element) {
-  console.log(element);
-  //toggle allows to auto show or remove show
-  element.classList.toggle("show");
-  response = element.innerText;
+rateButtons.forEach((btn, i) =>  {
+  console.log(i)
+  btn.addEventListener('click', e => {
+      rateButtons.forEach(btn => {
+          btn.classList.remove('show');
+      })
+      btn.classList.add('show');
+      submit.addEventListener('click', ev => {
+          ev.preventDefault();
+          // submitContainer.classList.add('submitted');
+          showResponse(e);
+      });
+  });
+})
 
-  console.log(response);
-}
 
-//submit button functionality
-
-submit.addEventListener("click", () => {
-  showResponse();
-});
-
-function showResponse() {
+function showResponse(e) {
+  console.log(e.target.value)
   thankYouCard.classList.remove("hide");
   ratingCard.classList.add("hide");
-  let showRating = `You selected ${response} out of 5`;
+  let showRating = `You selected ${e.target.textContent} out of 5`;
   return (showAnswer.innerText = showRating);
 }
